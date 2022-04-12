2a3afa2ed811f0a6e3baac4864ad495f72dc1f9e
dgosilva <davidgosilva@gmail.com>
2022-04-12 12:00:44 -0300

    changing pic url

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ import { Section } from "../styles/GlobalComponents";
 const title = "David Silva";
 const siteName = "David Silva's Portfolio";
 const description = "A Full Stack developer's portfolio";
+const thumbnailUrl = "https://www.facebook.com/photo/?fbid=4895367730555205&set=a.108773042548055"
 
 const Home = () => {
   return (
@@ -18,7 +19,7 @@ const Home = () => {
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
-        <meta property="og:image" content={'/images/htmltag.jpg'} />
+        <meta property="og:image" content={thumbnailUrl} />
         <meta property="og:site_name" content={siteName} />
         <meta property="og:type" content="website" />
         <link rel="icon" href="/images/htmltag.jpg" />
